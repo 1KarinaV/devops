289ec7e80d553180821f6deadc49a8361c2ab340
Dasxunya <79106085+dasxunya@users.noreply.github.com>
2025-09-12 00:17:44 +0300

    add sonarqube (#67)
    * add sonarqube

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     id("org.flywaydb.flyway") version "11.4.0"
     id("org.sonarqube") version "6.2.0.5505"
+    jacoco
 }
 
 group = "itmo"
@@ -13,6 +14,17 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 repositories {
@@ -60,9 +72,16 @@ flyway {
 
 sonar {
     properties {
-        property("sonar.projectKey", "Dasxunyasha_devops")
+        property("sonar.projectKey", "devops_backend")
         property("sonar.organization", "dasxunyasha")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.projectName", "Devops backend")
         property("sonar.token", System.getenv("SONAR_TOKEN"))
+//        property("sonar.qualitygate.wait", true)
+        property("sonar.coverage.jacoco.xmlReportPaths", "${buildDir}/reports/jacoco/test/jacocoTestReport.xml")
+    }
+
+    jacoco {
+        toolVersion = "0.8.8"
     }
 }
