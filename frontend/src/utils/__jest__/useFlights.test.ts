2f81c285b7b60f71abdaff4ab3a9062e621bf2e5
1KarinaV <red@itmo.ru>
2025-09-12 18:28:19 +0300

    feat(jenkins): Добавлены тесты на фронтенд

--- frontend/src/utils/__jest__/useFlights.test.ts
+++ frontend/src/utils/__jest__/useFlights.test.ts
@@ -118,6 +118,7 @@ describe("useFlights", () => {
         expect(result.current.flights).toEqual([]);
     });
 
+<<<<<<< HEAD
     // it("Обновление существующего рейса", async () => {
     //     const updatedFlight: Flight = {
     //         ...mockFlights[0],
@@ -208,4 +209,96 @@ describe("useFlights", () => {
     //     );
     //     expect(result.current.flights.find(f => f.id === "1")).toEqual(mockFlights[0]);
     // });
+=======
+    it("Обновление существующего рейса", async () => {
+        const updatedFlight: Flight = {
+            ...mockFlights[0],
+            destination: "Tokyo",
+        };
+
+        (global.fetch as jest.Mock).mockResolvedValueOnce({
+            ok: true,
+            json: async () => mockFlights,
+        });
+        const { result } = renderHook(() => useFlights());
+        await act(async () => {});
+
+        (global.fetch as jest.Mock).mockResolvedValueOnce({
+            ok: true,
+            json: async () => updatedFlight,
+        });
+
+        await act(async () => {
+            await result.current.updateFlight(updatedFlight);
+        });
+
+        expect(result.current.flights.find(f => f.id === "1")).toEqual(updatedFlight);
+    });
+
+    it("Вывод ошибку при неудачном обновлении рейса", async () => {
+        const updatedFlight: Flight = {
+            ...mockFlights[0],
+            destination: "Tokyo",
+        };
+
+        (global.fetch as jest.Mock).mockResolvedValueOnce({
+            ok: true,
+            json: async () => mockFlights,
+        });
+        const { result } = renderHook(() => useFlights());
+        await act(async () => {});
+
+        (global.fetch as jest.Mock).mockResolvedValueOnce({
+            ok: false,
+        });
+
+        await act(async () => {
+            await result.current.updateFlight(updatedFlight);
+        });
+
+        expect(console.error).toHaveBeenCalledWith(
+            "Ошибка при обновлении рейса:",
+            expect.any(Error)
+        );
+        expect(result.current.flights.find(f => f.id === "1")).toEqual(mockFlights[0]);
+    });
+
+    it("Удаляется рейс", async () => {
+        (global.fetch as jest.Mock).mockResolvedValueOnce({
+            ok: true,
+            json: async () => mockFlights,
+        });
+        const { result } = renderHook(() => useFlights());
+        await act(async () => {});
+
+        (global.fetch as jest.Mock).mockResolvedValueOnce({ ok: true });
+
+        await act(async () => {
+            await result.current.deleteFlight("1");
+        });
+
+        expect(result.current.flights.find(f => f.id === "1")).toBeUndefined();
+    });
+
+    it("Вывод ошибки при неудачном удалении рейса", async () => {
+        (global.fetch as jest.Mock).mockResolvedValueOnce({
+            ok: true,
+            json: async () => mockFlights,
+        });
+        const { result } = renderHook(() => useFlights());
+        await act(async () => {});
+
+        (global.fetch as jest.Mock).mockResolvedValueOnce({ ok: false });
+
+        await act(async () => {
+            await result.current.deleteFlight("1");
+        });
+
+        expect(console.error).toHaveBeenCalledWith(
+            "Ошибка при удалении рейса:",
+            expect.any(Error)
+        );
+        expect(result.current.flights.find(f => f.id === "1")).toEqual(mockFlights[0]);
+    });
+>>>>>>> b61fa50 (feat(jenkins): Добавлены тесты на фронтенд)
 });
